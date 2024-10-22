56a662239bf696aca4c09f8ea2bce9c0173da030
Senchenko Nikita <nikitos.senchenko@yandex.ru>
2024-10-22 15:34:32 +0300

    update

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3.10
 
 from flask import Flask, render_template
+from config import variables 
+from jinja2.exceptions import TemplateNotFound
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('base.html', **variables)
 
-@app.route('/plants')
-def page():
-    return render_template('plants.html')
-
-@app.route('/owner')
-def hello_world():
-    return 'Сенченко Никита Тестовое'
+@app.route('/<file_name>')
+def page(file_name):
+    try:
+        return render_template(f'{file_name}.html',**variables)
+    except TemplateNotFound:
+        abort(404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5001)
+    app.run(debug=True, host='0.0.0.0',port=5000)
---
+++ config.py
@@ -0,0 +1,4 @@
+variables = {
+    'year': 2024,
+    'email': 'nikitos.senchenko@yandex.ru'
+}
